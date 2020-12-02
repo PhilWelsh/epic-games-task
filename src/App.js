64abc7c1c7524b45d6e00aa435618a9ed4610143
unknown <phizs@hotmail.com>
2020-12-02 15:14:53 -0500

    styles improved, override Mui styles, mobile styles working, styles needs commenting

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.scss';
 function App() {
   
   return (
-    <div className="App" style={{maxWidth:600}}>
+    <div className="App theme">
       <header className="App-header">
-        <h1>Marvelous!</h1>
+        <h1><i>Marvelous!</i></h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
       </header>
       <ItemApp/>
--- src/ItemApp.js
+++ src/ItemApp.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import { nanoid } from 'nanoid'
-import { Button,Input,TextField,NativeSelect,InputLabel,MenuItem, IconButton, InputAdornment } from '@material-ui/core';
+import { Button,TextField,NativeSelect,IconButton } from '@material-ui/core';
 import CancelIcon from '@material-ui/icons/Cancel';
 import SearchIcon from '@material-ui/icons/Search';
 
@@ -24,57 +24,55 @@ const colData = [
             {name:"item3", id:6},
         ]
     },    
-    {
-        name:"column3",
-        id:103,
-        items:[
-            {name:"item1", id:7},
-        ]
-    }
+    // {
+    //     name:"column3",
+    //     id:103,
+    //     items:[
+    //         {name:"item1", id:7},
+    //     ]
+    // }
 ]
 
 const ItemApp =()=>{
     const [columnData, setColumnData] =useState(colData)
     const [searchTerm, setSearchTerm] = useState("")
 
-    const [values, setValues] = useState({
-        column:{...colData}
-    });
-    const [newEntry,setNewEntry] = useState("")
-    function handleNewEntryChange (e){
-        setNewEntry(e.target.value)
-    }
+    const [selectedColumnValue, setSelectedColumnValue] = useState();
+    const handleSelectChange = (event) => {
+        const name = event.target.name;
+        setSelectedColumnValue({
+          ...selectedColumnValue,
+          [name]: event.target.value,
+        });
+      };
+    
 
     const handleItemDelete = (e)=>{
         const currentTargetId = e.currentTarget.name 
         setColumnData(columnData.map(column => ({
             ...column,
             items: column.items
-            .filter(item => (item.id != currentTargetId))
+            .filter(item => item.id !== (Number(currentTargetId)))
         })))
     }
 
     const handleSearchChange = (event) =>{
         setSearchTerm(event.target.value)
     }
-  const handleChange = (event) => {
-    const name = event.target.name;
-    setValues({
-      ...values,
-      [name]: event.target.value,
-    });
-  };
 
-  const addItem = ()=>{
-      return (values.column)==="Choose Column" ? (
-          console.log("error")
+  const addItem = (event)=>{
+    event.preventDefault();
+    const newEntry = event.target[0].value
+    // const columnValue = selectedColumnValue?.column
+      return (!selectedColumnValue?.column || !event.target[0].value) ? (
+          console.log("error message function")
       ) : ( 
         setColumnData(prevState => (
         [...prevState.map(el => (
-            el.id == values.column ? { ...el, items: [...el.items, {name:newEntry, id:nanoid()}] } : el
+            el.id === (Number(selectedColumnValue.column)) ? { ...el, items: [...el.items, {name:newEntry, id:nanoid()}] } : el
             ))
         ]
-        )), setNewEntry("")
+        ))
     )
   }
 
@@ -83,16 +81,16 @@ const ItemApp =()=>{
             const Items = ()=> column.items.map(({...item}) => {
                 return item.name.includes(searchTerm) &&
                 (
-                <div key={item.id} style={{display:"grid", gridTemplateColumns: "1fr 50px"}}>
-                    <span style={{margin: "auto 2px"}}>{item.name}</span>
+                <div key={item.id} className="item-app-column-item">
+                    <span>{item.name}</span>
                     <IconButton aria-label="delete"  onClick={handleItemDelete} name={item.id} >
                         <CancelIcon/>
                     </IconButton>
                 </div>)
             })
                 return(
-                    <div key={column.name}>
-                        <div key={column.name}>{column.name}</div>
+                    <div className="item-app-column-container" key={column.name}>
+                        <h3 key={column.name}>{column.name}</h3>
                         <Items/>
                     </div>
                 )
@@ -100,44 +98,46 @@ const ItemApp =()=>{
         )
         
         return(
-            <div className="columns" style={{display:"grid", columnGap: "10px", gridAutoFlow: "column"}}>
+            <div className="item-app-columns">
                 <Columns/>
             </div>
         )
     }
 
     return(
-    <>
+    <div className="item-app">
         <h2>Add an item</h2>
-        <div style={{display:"grid", gridTemplateColumns:`1fr ${columnData.length}fr`}}>
-            <div className="column-entry">
-            <form>
-                <TextField onChange={handleNewEntryChange} value={newEntry}/>
-                <NativeSelect
-                value={values.column}
-                onChange={handleChange}
-                inputProps={{
-                    name: 'column',
-                    id: 'column-native-helper',
-                }}
-                >
-                <option aria-label="None" value="Choose Column" >Choose Column</option>
-                {columnData.map(column=>{
-                    return <option value={column.id} key={column.id}>{column.name}</option>
-                })}
-                </NativeSelect>
-                <Button color="primary" onClick={addItem}>Add Item</Button> 
+        <div className="item-app-grid" style={{gridTemplateColumns:`1fr ${columnData.length}fr`}}>
+            <div className="item-app-column-forms">
+            <form onSubmit={addItem}>
+                <div className="item-app-form-entry">
+                    <TextField variant="outlined" id="new-entry-input" label="Enter Item"/>
+                    {console.log(selectedColumnValue)}
+                    <NativeSelect
+                    variant="outlined"
+                    value={selectedColumnValue?.column}
+                    onChange={handleSelectChange}
+                    inputProps={{
+                        name: 'column',
+                        id: 'column-native-helper',
+                    }}
+                    >
+                    <option aria-label="None" value="" >Choose Column</option>
+                    {columnData.map(column=>{
+                        return <option value={column.id} key={column.id}>{column.name}</option>
+                    })}
+                    </NativeSelect>
+                </div>
+                <Button type="submit" variant="outlined">Add Item</Button> 
             </form>
-            <form>
-                <TextField id="standard-search" label="Search an Item" type="search" placeholder="search..." onChange={handleSearchChange}
-                InputProps={{
-                    startAdornment:(<SearchIcon />)
-                }}/>
-            </form>
+            <TextField id="standard-search" label="Search an Item" type="search" placeholder="search..." onChange={handleSearchChange} variant="outlined"
+            InputProps={{
+                startAdornment:(<SearchIcon />)
+            }}/>
             </div>
             <ColumnGrid colData={columnData} />
         </div>
-    </>
+    </div>
     )
 }
 
