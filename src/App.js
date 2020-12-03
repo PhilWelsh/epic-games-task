b81795999cd4a9460c27302725ec3c352553ff14
unknown <phizs@hotmail.com>
2020-12-03 09:06:30 -0500

    helmet for SEO added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import ItemApp from './ItemApp'
 import './App.scss';
+import {Helmet} from 'react-helmet'
+
 
 function App() {
   
-  return (
+  return (<>
+    <Helmet>
+      <title>Item App</title>
+      <meta name="description" content="An app for all your item in column needs" />
+    </Helmet>
     <div className="App theme">
       <header className="App-header">
         <h1><i>Marvelous!</i></h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
       </header>
-      <ItemApp/>
+      <section>
+        <ItemApp/>
+      </section>
     </div>
+    </>
   );
 }
 
